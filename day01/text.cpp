63e8c4b571d90ff2c5a36209f96b7df014b6d01a
wh <1575308742@qq.com>
2018-06-27 18:39:33 +0800

    命名空间/函数重载/引用

---
+++ day01/text.cpp
@@ -0,0 +1,54 @@
+//命名空间
+
+// #include<iostream>
+// 
+// namespace my{
+//     namespace your{
+//     int i = 1;
+//     }
+// }
+// 
+// 
+// int main()
+// {
+//     std::cout<<my::your::i<<std::endl;
+//     return 0;
+// }
+
+//函数重载
+//#include <iostream>
+//using namespace std;
+//
+//void FF(int i)
+//{
+//    cout<<i<<endl;
+//}
+//void FF(char c)
+//{
+//    cout<<c<<endl;
+//}
+//
+//int main()
+//{
+//    FF('c');
+//    return 0;
+//}
+
+//引用
+#include<iostream>
+using namespace std;
+int & FF( int i)
+{
+    cout<<i<<endl;
+    i++;
+    static int c = i;
+    return c;
+}
+
+int main()
+{
+    int i = 1;
+    int &a = FF(i);
+    cout<<a<<endl;
+    return 0;
+}
